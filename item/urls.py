c71270e1785893af75b11058145b6e830bdcc71b
Ambrose Musyoka <ambrosemusyoka254@gmail.com>
2024-04-10 00:03:16 +0300

    Version 1

--- item/urls.py
+++ item/urls.py
@@ -4,7 +4,10 @@ from .import views
 
 app_name= 'item'
 
-urlpatterns = [      
+urlpatterns = [    
+    path('', views.items, name='items'),  
     path('new/', views.new, name='new'),
     path('<int:pk>/',views.detail, name='detail'),
+    path('<int:pk>/delete/',views.delete, name='delete'),
+     path('<int:pk>/edit/',views.edit, name='edit'),
 ]
